d07def3bd5bf569592068b1c6d32d6a0ecc96927
masteryi-0018 <1536474741@qq.com>
2025-12-18 22:10:25 +0800

    [feat] add pre-commit of clang-format

---
+++ docs/01_dev.md
@@ -0,0 +1,12 @@
+## dev
+
+in linux
+
+### code
+
+1. cpp
+```sh
+pip install pre-commit
+pre-commit install
+pre-commit run clang-format --all-files
+```
--- src/core/hello.cpp
+++ src/core/hello.cpp
@@ -1,10 +1,11 @@
 #include "hello.h"
-#include "Eigen/Core"
-#include <iostream>
+
 #include <Eigen/Dense>
+#include <iostream>
+
+#include "Eigen/Core"
 
 void say_hello() {
   std::cout << "Hello from core library" << std::endl;
   Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> m;
 }
-
